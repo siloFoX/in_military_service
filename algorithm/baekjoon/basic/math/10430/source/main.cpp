65cb7e51921e216b2eff9abe6cd9ca25028e9eb7
silofox <ladnav0704@gmail.com>
2023-04-30 10:50:24 +0000

    Make draft first problem

--- algorithm/baekjoon/basic/math/10430/source/main.cpp
+++ algorithm/baekjoon/basic/math/10430/source/main.cpp
@@ -1,1 +1,18 @@
-#include <stdio.h>;
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // ios_base::sync_with_studio(false);
+  // cin.tie(NULL);
+
+  int A, B, C;
+  cin >> A >> B >> C;
+
+  cout << (A + B) % C << endl;
+  cout << ((A % C) + (B % C)) % C << endl;
+  cout << (A * B) % C << endl;
+  cout << ((A % C) * (B % C)) % C;
+
+  return 0;
+}
---
+++ algorithm/baekjoon/basic/math/10430/source/make_excutable_and_compare_with_testcase.sh
@@ -0,0 +1,7 @@
+g++ main.cpp -o test/main
+./test/main < test/input > test/output_testcase
+diff test/output test/output_testcase
+
+echo "---"
+echo "If nothing happend, testcase works. congratulations!"
+echo "---"
